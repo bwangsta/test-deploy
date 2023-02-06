01437837c3a6c4691efae7943f8cd189756233af
Brian Wang <bwbrianwang1018@gmail.com>
2023-02-05 16:02:50 -0800

    Test deployment 1

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <h1>Hello World</h1>
+      <h2>Testing Deployment</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
